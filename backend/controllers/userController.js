7a00eef18ea866266b54cf904f53f09ed04b986b
Patrik Zlatic <patrik.zlatic@gmail.com>
2025-02-08 20:09:26 +0100

    Update cookie-settings

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -189,8 +189,8 @@ export const loginUser = async (req, res, next) => {
       .status(200)
       .cookie("jwt", token, {
         httpOnly: true,
-        sameSite: "Lax",
-        // secure: true,
+        sameSite: "none",
+        secure: true,
         maxAge: 24 * 60 * 60 * 1000, // 1 day in miliseconds
       })
       .json({ msg: "Successfully signed in" });
