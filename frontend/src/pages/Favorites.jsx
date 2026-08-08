eec73bbbd344250c05cceeaaab69b5115c214fa3
Patrik <patrik.zlatic@gmail.com>
2026-08-08 12:44:51 +0200

    refactor: extract RecipeCollection as component from Favorites page

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,61 +1,16 @@
-import { useContext, useState, useRef } from "react";
+import { useContext } from "react";
 import { RecipeContext } from "../context/RecipeContext";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFire,
-  faHeart,
-  faShoppingCart,
-  faCarrot,
-  faBasketShopping,
-} from "@fortawesome/free-solid-svg-icons";
-import { updateFavoritesDatabase } from "../api/favorites";
-import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { GrFavorite } from "react-icons/gr";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import {
-  RiCheckboxBlankCircleLine,
-  RiCheckboxCircleLine,
-} from "react-icons/ri";
-import { LuLeaf } from "react-icons/lu";
-import { AiOutlineFire } from "react-icons/ai";
-import { useEffect } from "react";
-
-const DIET_OPTIONS = ["vegetarian", "vegan", "dairy-free", "gluten-free"];
+import RecipeCollection from "../components/RecipeCollection";
+import { updateFavoritesDatabase } from "../api/favorites";
+import toast from "react-hot-toast";
 
 function Favorites() {
   const { favorites, setFavorites } = useContext(RecipeContext);
-  const [isDietDropdownOpen, setIsDietDropdownOpen] = useState(false);
-  const dietDropdownRef = useRef(null);
-  const [diet, setDiet] = useState([]);
-  const [isCaloriesDropdownOpen, setIsCaloriesDropdownOpen] = useState(false);
-  const caloriesDropdownRef = useRef(null);
-  const [isPer100g, setIsPer100g] = useState(false);
-  const [maxCalories, setMaxCalories] = useState(600);
   const navigate = useNavigate();
 
-  // Close calories / diet dropdown when clicking outside
-  useEffect(() => {
-    const handleClickOutside = (e) => {
-      if (
-        caloriesDropdownRef.current &&
-        !caloriesDropdownRef.current.contains(e.target)
-      ) {
-        setIsCaloriesDropdownOpen(false);
-      }
-      if (
-        dietDropdownRef.current &&
-        !dietDropdownRef.current.contains(e.target)
-      ) {
-        setIsDietDropdownOpen(false);
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
+  // Remove from UI immediately, if backend call fails revert to previous state
   const handleRemoveFromFavorites = async (e, favoriteRecipeId) => {
     e.stopPropagation();
     const previousFavorites = favorites;
@@ -76,230 +31,15 @@ function Favorites() {
     }
   };
 
-  const filteredFavorites = favorites.filter((recipe) => {
-    if (!recipe.nutritionPer100g || !recipe.nutritionPerServing || !recipe.diet)
-      return false;
-
-    if (
-      maxCalories < 600 &&
-      isPer100g &&
-      recipe.nutritionPer100g.calories > maxCalories
-    )
-      return false;
-    if (
-      maxCalories < 600 &&
-      !isPer100g &&
-      recipe.nutritionPerServing.calories > maxCalories
-    )
-      return false;
-    if (diet.includes("vegetarian") && !recipe.diet.vegetarian) return false;
-    if (diet.includes("vegan") && !recipe.diet.vegan) return false;
-    if (diet.includes("gluten-free") && !recipe.diet.glutenFree) return false;
-    if (diet.includes("dairy-free") && !recipe.diet.dairyFree) return false;
-
-    return true;
-  });
-
   return (
     <>
       {favorites.length > 0 ? (
-        <div className="mx-auto md:max-w-[90%] lg:max-w-[1400px] w-full px-1 sm:px-6 pb-2 min-h-full text-gray-300 self-start">
-          <div className="rounded-3xl w-full mx-auto p-4 flex flex-col md:flex-row justify-between gap-4">
-            <h1 className="text-3xl font-bold text-center text-orange-100">
-              Your Top Picks
-            </h1>
-            {/* Filter section */}
-            <div className="flex flex-col sm:flex-row items-center justify-center gap-3">
-              {/** Calories */}
-              <div className="relative" ref={caloriesDropdownRef}>
-                <div
-                  className="flex items-center border gap-2 border-gray-600 hover:border-gray-400 active:scale-[0.98] transition-all rounded-full  px-5 py-2.5 cursor-pointer select-none"
-                  onClick={() => setIsCaloriesDropdownOpen((prev) => !prev)}
-                >
-                  <AiOutlineFire
-                    className={`${maxCalories < 600 ? "text-orange-400" : "text-orange-400/70"}`}
-                  />
-                  <span
-                    className={`${maxCalories < 600 ? "text-orange-200" : "text-gray-300"}`}
-                  >
-                    Calories
-                  </span>
-                  <MdKeyboardArrowDown
-                    className={`size-5 ${maxCalories < 600 ? "text-orange-200" : ""}`}
-                  />
-                </div>
-                {isCaloriesDropdownOpen && (
-                  <div className="absolute min-w-max gap-1 left-1/2 -translate-x-1/2 mt-1 flex flex-col z-10 bg-black border border-gray-600 rounded-3xl py-5 px-5">
-                    <div className="grid grid-cols-2 bg-gray-900 rounded-full">
-                      <button
-                        className={`text-sm rounded-full px-6 py-2 text-center ${isPer100g ? "bg-orange-200 text-gray-800" : ""} transition-all`}
-                        onClick={() => setIsPer100g(true)}
-                      >
-                        Per 100g
-                      </button>
-                      <button
-                        className={`text-sm rounded-full px-6 py-2 text-center ${!isPer100g ? "bg-orange-200 text-gray-800" : ""} transition-all`}
-                        onClick={() => setIsPer100g(false)}
-                      >
-                        Per serving
-                      </button>
-                    </div>
-
-                    <div className="flex justify-between pt-5 pb-1 text-sm">
-                      <span>0 kcal</span>
-                      <span>
-                        <span className="text-orange-100">{maxCalories}</span>
-                        {maxCalories >= 600 && "+"} kcal
-                      </span>
-                    </div>
-
-                    <input
-                      type="range"
-                      min="0"
-                      max="600"
-                      step="50"
-                      value={maxCalories}
-                      onChange={(e) => setMaxCalories(+e.target.value)}
-                      className="w-full accent-orange-200"
-                    />
-                  </div>
-                )}
-              </div>
-
-              <div className="relative" ref={dietDropdownRef}>
-                <div
-                  className="flex items-center border gap-2 border-gray-600 hover:border-gray-400 active:scale-[0.98] transition-all rounded-full px-5 py-2.5 cursor-pointer select-none"
-                  onClick={() => setIsDietDropdownOpen((prev) => !prev)}
-                >
-                  <LuLeaf
-                    className={`${diet.length > 0 ? "text-green-500" : "text-green-500/70"}`}
-                  />
-                  <span
-                    className={`${diet.length > 0 ? "text-orange-200" : ""}`}
-                  >
-                    Diet
-                  </span>
-                  <span
-                    className={`text-xs flex items-center justify-center rounded-full bg-green-700 text-white size-5 text-center ${diet.length > 0 ? "opacity-100" : "opacity-0"}`}
-                  >
-                    {diet.length}
-                  </span>
-                  <MdKeyboardArrowDown
-                    className={`size-5 ${diet.length > 0 ? "text-orange-200" : ""}`}
-                  />
-                </div>
-                {isDietDropdownOpen && (
-                  <div className="absolute min-w-max gap-1 mt-1 left-1/2 -translate-x-1/2 flex flex-col z-10 bg-black border border-gray-600 rounded-3xl py-3 px-2">
-                    {DIET_OPTIONS.map((option) => {
-                      const isSelected = diet.includes(option);
-                      return (
-                        <button
-                          key={option}
-                          className={`flex items-center gap-3 pl-3 pr-9 py-1.5 rounded-xl ${isSelected ? "text-orange-100" : ""}`}
-                          onClick={() =>
-                            isSelected
-                              ? setDiet((prev) =>
-                                  prev.filter((x) => x !== option),
-                                )
-                              : setDiet((prev) => [...prev, option])
-                          }
-                        >
-                          <div>
-                            {isSelected ? (
-                              <RiCheckboxCircleLine className="size-5 text-green-700" />
-                            ) : (
-                              <RiCheckboxBlankCircleLine className="size-5 text-gray-600" />
-                            )}
-                          </div>{" "}
-                          <span className="text-sm capitalize">{option}</span>
-                        </button>
-                      );
-                    })}
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-
-          <div className="grid grid-cols-[repeat(auto-fit,minmax(140px,200px))] sm:grid-cols-[repeat(auto-fit,minmax(280px,312px))] justify-center gap-5 sm:gap-6">
-            {filteredFavorites.map((recipe) => (
-              <div
-                key={recipe.id}
-                onClick={() =>
-                  recipe.id && navigate(`/home/recipe-details/${recipe.id}`)
-                }
-                className="border border-gray-800 hover:border-orange-200/40 bg-gray-950 rounded-3xl active:scale-[0.98] overflow-hidden shadow-lg hover:scale-[1.03] transition-all duration-300 cursor-pointer flex flex-col relative group w-full max-w-[280px] sm:max-w-none justify-self-center"
-              >
-                <button
-                  className="absolute top-3 right-3 bg-black/50 backdrop-blur-sm p-2 rounded-full hover:bg-opacity-75 hover:scale-105 transition duration-300 text-red-500 lg:opacity-0 lg:group-hover:opacity-100"
-                  onClick={(e) => handleRemoveFromFavorites(e, recipe.id)}
-                >
-                  <FontAwesomeIcon icon={faHeart} size="xl" />
-                </button>
-                <img
-                  src={recipe.image}
-                  alt={recipe.title}
-                  className="w-full object-cover aspect-[312/231]"
-                />
-                <div className="flex flex-col h-full justify-between p-3 sm:p-4">
-                  <h2
-                    className={`${
-                      recipe.title.length > 36
-                        ? "text-base"
-                        : "text-lg sm:text-xl"
-                    } font-semibold mb-2`}
-                  >
-                    {recipe.title}
-                  </h2>
-                  <div>
-                    {isPer100g
-                      ? recipe.nutritionPer100g?.calories && (
-                          <div className="flex items-center gap-2 text-sm">
-                            <FontAwesomeIcon
-                              icon={faFire}
-                              className="text-red-700/80 size-4"
-                            />
-                            <span>
-                              {Math.round(recipe.nutritionPer100g.calories)}{" "}
-                              kcal <span className="text-xs">/ 100g</span>
-                            </span>
-                          </div>
-                        )
-                      : recipe.nutritionPerServing?.calories && (
-                          <div className="flex items-center gap-2 text-sm">
-                            <FontAwesomeIcon
-                              icon={faFire}
-                              className="text-red-700/80 size-4"
-                            />
-                            <span>
-                              {Math.round(recipe.nutritionPerServing.calories)}{" "}
-                              kcal <span className="text-xs">/ serving</span>
-                            </span>
-                          </div>
-                        )}
-
-                    <span className="flex text-sm items-center gap-2">
-                      <FontAwesomeIcon
-                        icon={faCarrot}
-                        className="size-4 text-orange-600/80"
-                      />{" "}
-                      {recipe.ingredients.length} ingredients
-                    </span>
-                    {recipe.missedIngredientCount > 0 && (
-                      <span className="flex items-center gap-2">
-                        <FontAwesomeIcon
-                          icon={faBasketShopping}
-                          className="size-4"
-                        />{" "}
-                        {recipe.missedIngredientCount} missing
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <RecipeCollection
+          recipesSource={favorites}
+          heading={"Your Top Picks"}
+          showFavoritesControl
+          handleRemoveFromFavorites={handleRemoveFromFavorites}
+        />
       ) : (
         <div className="flex flex-col w-full max-w-2xl rounded-3xl justify-center items-center p-10 m-2 border border-gray-800 bg-[#11151E] text-center">
           <GrFavorite className="size-10 sm:size-12 text-orange-200 mb-4 sm:mb-3" />
